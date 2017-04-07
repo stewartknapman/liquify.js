127af4aa30bc4cc3737456999792939ea49ff64f
Stewart Knapman <info@stewartknapman.com>
2017-04-08 11:27:44 +1200

    cleaning up test; working it out

--- tests/intergration/liquify.js
+++ tests/intergration/liquify.js
@@ -1,3 +1,34 @@
+/*
+
+  Setup new liquid object
+  class accepts:
+   - custom tag drops
+   - custom filter drops
+   - custom method for finding includes
+
+  var liquify = new Liquify({
+    tags: {
+      newTag: function () {} // TODO: params and return value
+    },
+    filters: {
+      newFilter: function () {} // TODO: params and return value
+    },
+    include: function () {} // TODO: params and return value
+  });
+
+  Parse template
+  (seperate from render so you can reuse the template fast? does this help/matter?
+   I guess we don't want to be regexing on the string everytime we want to render)
+
+  var template = liquify.parse('hello {{ user.name }}');
+
+  Render template
+  output => 'hello Stewart'
+  
+  template.render({ "user": { "name": "Stewart" } });
+
+*/
+
 require('jsdom-global')();
 var fs = require('fs');
 var assert = require('chai').assert;
@@ -12,31 +43,3 @@ describe('Liquify.js', function() {
   it('renders a liquid template');
 
 });
-
-/*
-
-  // Setup new liquid object
-  // class accepts:
-  //  - custom tag drops
-  //  - custom filter drops
-  //  - custom method for finding includes
-  var liquify = new Liquify({
-    tags: {
-      newTag: function () {} // TODO: params and return value
-    },
-    filters: {
-      newFilter: function () {} // TODO: params and return value
-    },
-    include: function () {} // TODO: params and return value
-  });
-
-  // Parse template
-  // (seperate from render so you can reuse the template fast? does this help/matter?
-  //  I guess we don't want to be regexing on the string everytime we want to render)
-  var template = liquify.parse('hello {{ user.name }}');
-
-  // Render template
-  // output => 'hello Stewart'
-  template.render({ user: { name: 'Stewart' } });
-
-*/
