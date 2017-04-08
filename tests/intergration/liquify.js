bc57b66c345d4ed103d5f8251dbeca079543b956
Stewart Knapman <info@stewartknapman.com>
2017-04-08 15:50:37 +1200

    tokenise template string

--- tests/intergration/liquify.js
+++ tests/intergration/liquify.js
@@ -49,6 +49,22 @@ describe('Liquify.js', function() {
     assert.equal(output, 'hello Stewart');
   });
 
-  it('renders a complex liquid template');
+  it('renders a complex liquid template', function () {
+    var src = document.querySelector('#input').innerHTML;
+    var expectedOutput = document.querySelector('#output').innerHTML;
+    var data = {
+      "products": [
+        { "title": "Example Product 1" },
+        { "title": "Example Product 2" },
+        { "title": "Example Product 3" }
+      ]
+    };
+
+    var liquify = new Liquify();
+    var template = liquify.parse(src);
+    var output = template.render(data);
+
+    // assert.equal(output, expectedOutput); TODO: this!
+  });
 
 });
