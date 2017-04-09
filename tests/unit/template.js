270050df953bba99abf419c8c5db48684c029e69
Stewart Knapman <info@stewartknapman.com>
2017-04-10 08:57:58 +1200

    extract parsers

--- tests/unit/template.js
+++ tests/unit/template.js
@@ -1,11 +1,12 @@
 var assert = require('chai').assert;
 var Template = require('../../lib/liquify/template.js');
+var parsers = require('../../lib/liquify/parsers.js');
 
 describe('Template', function() {
 
   it('accepts a template string to its constructor', function () {
     var src = 'hello {{ user.name }}';
-    var template = new Template(src);
+    var template = new Template(src, parsers);
 
     assert.equal(template.src, src);
   });
@@ -20,14 +21,14 @@ describe('Template', function() {
 
   it('has a render method', function () {
     var src = 'hello {{ user.name }}';
-    var template = new Template(src);
+    var template = new Template(src, parsers);
     assert.typeOf(template.render, 'function');
   });
 
   it('accepts a json object to its render method', function () {
     var output;
     var src = 'hello {{ user.name }}';
-    var template = new Template(src);
+    var template = new Template(src, parsers);
 
     output = template.render();
     assert.equal(output, undefined);
@@ -44,7 +45,7 @@ describe('Template', function() {
 
   it('returns a rendered liquid template', function () {
     var src = 'hello {{ user.name }}';
-    var template = new Template(src);
+    var template = new Template(src, parsers);
     var output = template.render({ "user": { "name": "Stewart" } });
 
     assert.typeOf(output, 'string');
