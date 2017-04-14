3e61ff41bf6ab6b783be3a084cbd9f730dabe43d
Stewart Knapman <info@stewartknapman.com>
2017-04-14 14:03:06 +1200

    tests for tokens

--- tests/unit/tokens.js
+++ tests/unit/tokens.js
@@ -5,15 +5,29 @@ var parsers = new Parsers();
 
 describe('Tokens', function() {
 
-  it('accepts a template string to its constructor');
+  it('tokenizes the string', function () {
+    var tokens = new Tokens('hello {{ user.name }}', parsers);
+    var expected = ['hello ', '{{ user.name }}'];
 
-  it('tokenizes the string');
+    assert.equal(tokens.tokens[0], expected[0]);
+    assert.equal(tokens.tokens[1], expected[1]);
+  });
 
   describe('shift()', function () {
 
-    it('has a shift method');
+    it('has a shift method', function () {
+      var tokens = new Tokens('', parsers);
 
-    it('returns a token');
+      assert.typeOf(tokens.shift, 'function');
+    });
+
+    it('returns a token', function () {
+      var tokens = new Tokens('hello {{ user.name }}', parsers);
+      var expected = ['hello ', '{{ user.name }}'];
+
+      assert.equal(tokens.shift(), expected[0]);
+      assert.equal(tokens.shift(), expected[1]);
+    });
 
   });
 
