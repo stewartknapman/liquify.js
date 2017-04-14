bd0ca0b41ac94dce7bb7a62be6967e5509b6858e
Stewart Knapman <info@stewartknapman.com>
2017-04-14 12:41:54 +1200

    extract tokens

---
+++ tests/unit/tokens.js
@@ -0,0 +1,19 @@
+var assert = require('chai').assert;
+var Tokens = require('../../lib/liquify/tokens.js');
+var parsers = require('../../lib/liquify/parsers.js');
+
+describe('Tokens', function() {
+
+  it('accepts a template string to its constructor');
+
+  it('tokenizes the string');
+
+  describe('shift()', function () {
+
+    it('has a shift method');
+
+    it('returns a token');
+
+  });
+
+});
